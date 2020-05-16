afa7b49f2f7e4fc9b5dcb9bb3dd990823983e1f5
mnskt <mantas.milieska@gmail.com>
2020-05-16 07:37:31 +0300

    Added playing board structure, board printing function and option to select where to input 'X'

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,20 @@
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
+theboard = {
+    'topLeft': ' ', 'topMid': ' ', 'topRight': ' ',
+    'midLeft': ' ', 'midMid': ' ', 'midRight': ' ',
+    'botLeft': ' ', 'botMid': ' ', 'botRight': ' '
+}
+
+def boardPrint(board):
+    print(board['topLeft'] + '|' + board['topMid'] + '|' + board['topRight'])
+    print('------')
+    print(board['midLeft'] + '|' + board['midMid'] + '|' + board['midRight'])
+    print('------')
+    print(board['botLeft'] + '|' + board['botMid'] + '|' + board['botRight'])
+
+isRunning = True
+
+while isRunning:
+    choice = input("Please enter the position where you want to place 'X'\nAvailable options: topLeft, topMid, topRight, midLeft, midMid, midRight, botLeft, botMid, botRight ")
+    theboard[choice] = 'X'
+    boardPrint(theboard)
+    break
